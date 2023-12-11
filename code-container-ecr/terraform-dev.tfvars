9029a0dd82b16bf12b4e42e97655809cffce02c6
Carlos Egito <carlos.egito@panago.com>
2023-12-11 13:41:28 -0800

    Adding codedeploy

--- code-container-ecr/terraform-dev.tfvars
+++
@@ -1,3 +0,0 @@
-codecommit_repo_default_branch_name = "main"
-codepipeline_artifacts_s3_bucket_name = "caduegito-cicd-codepipeline-artifacts"
-image_tag_prefix = "latest"
--- code-container-ecr/tf.sh
+++ code-container-ecr/tf.sh
@@ -6,23 +6,27 @@ ENV=$3
 
 export TF_VAR_env=${ENV}
 export TF_VAR_project_name=${PROJECT_NAME}
+export TF_VAR_image_tag_prefix=${PROJECT_NAME}
 export TF_VAR_codecommit_repo_name=${PROJECT_NAME}_${ENV}
 export TF_VAR_ecr_repo_name=${PROJECT_NAME}_${ENV}
-export TF_VAR_codebuild_project_name=${PROJECT_NAME}_${ENV}_imagebuild
+export TF_VAR_codebuild_project_name=${PROJECT_NAME}_${ENV}_project
 export TF_VAR_codebuild_service_role_name=${PROJECT_NAME}-${ENV}-codebuild-service-role
-export TF_VAR_codepipeline_pipeline_name=${PROJECT_NAME}_${ENV}_imagebuild_pipeline
+export TF_VAR_codepipeline_pipeline_name=${PROJECT_NAME}_${ENV}_project_pipeline
 export TF_VAR_codepipeline_role_name=${PROJECT_NAME}_${ENV}_codepipeline_role
 export TF_VAR_codepipeline_role_policy_name=${TFVAR_codepipeline_role_name}_policy
 export TF_VAR_cloudwatch_events_role_name=${PROJECT_NAME}_${ENV}_cloudwatch_events_role
 export TF_VAR_cloudwatch_events_role_policy_name=${TFVAR_cloudwatch_events_role_name}_policy
-export TF_VAR_cloudwatch_events_rule_name=${PROJECT_NAME}_${ENV}_trigger_imagebuild_pipeline
-export TF_VAR_codebuild_cloudwatch_logs_group_name=${PROJECT_NAME}_${ENV}_imagebuildloggroup
-export TF_VAR_codebuild_cloudwatch_logs_stream_name=${PROJECT_NAME}_${ENV}_imagebuildlogstream
+export TF_VAR_cloudwatch_events_rule_name=${PROJECT_NAME}_${ENV}_trigger_project_pipeline
+export TF_VAR_codebuild_cloudwatch_logs_group_name=${PROJECT_NAME}_${ENV}_project_loggroup
+export TF_VAR_codebuild_cloudwatch_logs_stream_name=${PROJECT_NAME}_${ENV}_project_logstream
+
+export TF_VAR_codecommit_repo_default_branch_name="main"
+export TF_VAR_codepipeline_artifacts_s3_bucket_name="caduegito-cicd-codepipeline-artifacts"
 
 case ${command} in
     plan )
        terraform init 
-       terraform plan -no-color -var-file=./terraform-${TF_VAR_env}.tfvars -out latest-${TF_VAR_env}.tfplan
+       terraform plan -no-color -out latest-${TF_VAR_env}.tfplan
        ;;
     apply )
        terraform init 
@@ -34,7 +38,7 @@ case ${command} in
        ;;
     destroy )
        terraform init
-       terraform destroy -auto-approve -no-color -var-file=./terraform-${TF_VAR_env}.tfvars
+       terraform destroy -auto-approve -no-color
        ;;
     show )
        terraform init
